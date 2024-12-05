e37a92a09b1700b75d22b970755e571e520dffb1
NexusGKSoftwares <nexusgk3@gmail.com>
2024-12-05 23:37:02 +0300

    "Added JavaScript code to handle form submission"

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to handle form submission
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+});
